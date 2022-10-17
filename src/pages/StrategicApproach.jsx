47772b5338fdd731d55830476ae55f29af47181e
Kara Thurmond <kthurmond@Karas-Air.lan>
2022-10-17 07:27:07 -0400

    adds photos

--- src/pages/StrategicApproach.jsx
+++ src/pages/StrategicApproach.jsx
@@ -1,5 +1,123 @@
 const StrategicApproach = () => {
-  return <div>strategicApproach</div>;
+  return (
+    <div>
+      <div className="relative max-w-screen-xl mx-auto relative grid grid-cols-2 md:grid-cols-4 px-5 md:px-0 mt-10 mb-5 font-light text-sm">
+        <div className="hidden md:block"></div>
+        <div className="col-span-2 flex flex-col">
+          <div className="text-primary font-bold">Dekker Babian Design</div>
+          <div className="text-secondary font-bold">
+            Our Approach is Strategic
+          </div>
+          <p className="pb-4">
+            We believe you must define yourself before you are defined by
+            others.
+          </p>
+          <p className="pb-4">
+            Connecting with an audience requires agility, discipline, and
+            ingenuity. We are ever mindful of both the macro and micro
+            challenges that organizations face today. Our design solutions work
+            for clients large and small, profit and nonprofit. You can expect
+            work that is creative, professional and executed flawlessly.{" "}
+          </p>
+          <p className="pb-4">
+            Dekker Babian understands the important issues in branding strategy,
+            identity and marketing communications.
+          </p>
+          <div className="text-secondary font-bold">
+            Our Founders are Analytical and Creative
+          </div>
+          <p className="pb-4">
+            Design team Marla Dekker and Kevork Babian, the founders of Dekker
+            Babian Design, headquartered in southeastern CT.
+          </p>
+          <div className="text-secondary font-bold">Marla Dekker</div>{" "}
+          <p className="pb-4">
+            Marla is a designer who relishes the opportunity to work in diverse
+            mediums—from electronic to print to environmental. Earlier in
+            Marla’s design career she worked at Pentagram Design in New York and
+            London, George Nelson Design and Siegel & Gale. Marla’s design
+            skills are enhanced by her knowledge of data analytics, web
+            development, social media and business development. Marla is the
+            Creative Director on projects for Management Consultants, Financial
+            Services firms and large events for Bike New York and the National
+            Book Awards.{" "}
+          </p>
+          <div className="text-secondary font-bold">Kevork Babian</div>{" "}
+          <p className="pb-4">
+            Kevork’s passion for both modern and classic design was fostered
+            growing up in Europe and the U.S. and receiving his education at
+            Cooper Union in New York City and in Basel, Switzerland. Before
+            founding Dekker Babian, Kevork created identity programs and
+            marketing communications for clients including NASA, Champion
+            Papers, the FAA, and Ward Bennett for Brickell Furniture. Kevork is
+            Dekker Babian’s Creative Director for Financial Services,
+            Healthcare, Pharmaceutical and Nonprofit clients.
+          </p>
+          <div className="text-secondary font-bold">
+            Townhouse.bz - a Dekker Babian business
+          </div>
+          <p className="pb-4">
+            In 2010, Dekker Babian launched Townhouse.bz as an opportunity to
+            sell fabulous mid-century modern furniture, objects and art, and
+            also as a forum for an on-going discussion about the aesthetics,
+            history, and abiding significance of modernist design. We aim to
+            mesh high style, value, and sustainability in a manner that
+            addresses broader concerns about the environmental and economic
+            challenges faced by the world today. Townhouse’s successful launch
+            was heralded by the New York Times Home section. Through
+            Townhouse.bz, Dekker Babian develops websites, social media and
+            print catalogs for artists as well as curates art and design events.
+          </p>
+          <a
+            href="mailto:marla@dekkerbabian.com"
+            className="block font-bold text-primary mb-10"
+          >
+            Contact us for a conversation or any inquiries.
+          </a>
+        </div>
+      </div>
+      <div className="relative max-w-screen-xl mx-auto flex justify-center">
+        <img
+          src="./images/marla.jpg"
+          className="max-w-[200px] sm:max-w-[400px]"
+        />
+        <img
+          src="./images/kevork.jpg"
+          className="max-w-[200px] sm:max-w-[400px]"
+        />
+      </div>
+      <div className="relative max-w-screen-xl mx-auto grid grid-cols-2 md:grid-cols-4">
+        <div className="block"></div>
+        <div className="col-span-2 flex space-x-6 p-5 lg:p-10 bg-secondary text-[#fff] font-light text-sm">
+          <p className="px-3 sm:px-10">
+            Marla Dekker
+            <br />
+            Founder, <br />
+            Dekker Babian Design
+            <br />
+            <br />
+            Photographed on location at Taliesin West, AZ, engaged in producing
+            a client’s recruiting video .
+          </p>
+          <p className="px-3 md:px-10">
+            Kevork Babian
+            <br />
+            Founder
+            <br />
+            Dekker Babian Design
+            <br />
+            <br />
+            Photographed at the opening of the Faux Real Salon Style exhibition,
+            a Townhouse.bz event.
+            <br />
+            <br />
+            Kevork is holding a rare Carl Aubock surreal brass bottle opener,
+            Australian, 1950s.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StrategicApproach;
