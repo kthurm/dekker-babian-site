33873e7ae8fd04f0aa2f10471f0e7fa127cc1698
Kara Thurmond <kthurmond@Karas-Air.lan>
2022-10-17 06:46:28 -0400

    Adds footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import Logo from "../images/DekkerBabianNavBar.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="w-full shadow-footer z-50">
+        <div className="max-w-screen-xl mx-auto py-2.5 px-5 grid grid-cols-2 md:grid-cols-4">
+          <div className="flex items-center space-x-3">
+            <button className="flex flex-col space-y-2.5">
+              <div className="w-12 h-0.5 bg-primary"></div>
+              <div className="w-12 h-0.5 bg-primary"></div>
+              <div className="w-12 h-0.5 bg-primary"></div>
+            </button>
+
+            <img src={Logo} alt="DekkerBabian Logo" className="w-[200px]" />
+          </div>
+          <div className="hidden md:block"></div>
+          <div className="px-5 md:px-1 md:w-[200px] text-sm leading-[17px]">
+            Would you like information or do you have a question?
+            <a
+              href="mailto:marla@dekkerbabian.com"
+              className="block text-primary"
+            >
+              Contact Marla Dekker
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
