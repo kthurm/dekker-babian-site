1b0b432430e3bc426859677869ed6814f5be0713
Kara Thurmond <kthurmond@Karas-Air.lan>
2022-10-17 06:48:36 -0400

    Adds content

--- src/pages/MeetOurClients.jsx
+++ src/pages/MeetOurClients.jsx
@@ -1,7 +1,101 @@
-import React from "react";
-
 const MeetOurClients = () => {
-  return <div>meetOurClients</div>;
-};
+  return (
+    <div className="relative max-w-screen-xl mx-auto grid grid-cols-2 md:grid-cols-4 font-light text-sm my-10">
+      <div className="hidden md:block"></div>
 
+      <div className="col-span-2 flex flex-col">
+        <div className="text-primary font-bold">Clients</div>
+        <div className="flex flex-col pb-8">
+          <p className="text-secondary font-bold">
+            Clients Marketing, Advertising &amp; Capabilities Campaigns
+          </p>
+          <p>Delta Consulting Group</p>
+          <p>Galt and Company</p>
+          <p>Goldman Sachs &amp; Co</p>
+          <p>Latin American Re</p>
+          <p>Mary McDowell School</p>
+          <p>J.P. Morgan</p>
+          <p>Prebon Yamane</p>
+          <p>ROC Laboratories</p>
+          <p>Society of Corporate Secretaries &amp; Governance Professionals</p>
+          <p>Marakon Associates Memorial</p>
+          <p>Sloan-Kettering Cancer Center</p>
+          <p>New York Hospital, Weill Cornell Medical Center</p>
+          <p>New York Downtown Hospital</p>
+          <p>Richmond Events</p>
+          <p>Townhouse.bz</p>
+          <p>US Trust</p>
+        </div>
+        <div className="flex flex-col pb-8">
+          <p className="text-secondary font-bold">
+            Web, Electronic, Video &amp; Social Media
+          </p>
+          <p>Bike New York</p>
+          <p>Galt and Company</p>
+          <p>Goldman Sachs &amp; Co</p>
+          <p>Grand Avenue Workshop</p>
+          <p>Jud Nelson, Sculptor</p>
+          <p>Kamilla Talbot, Getting Outside</p>
+          <p>Melanie Freundlich Lighting Design</p>
+          <p>MGM Design</p>
+          <p>National Book Foundation</p>
+          <p>Poems in Community</p>
+          <p>Susan Rowland, Uncontained Forces</p>
+          <p>Townhouse.bz</p>
+        </div>
+        <div className="flex flex-col pb-8">
+          <p className="text-secondary font-bold">
+            Branding Strategy, Logotype &amp; Identity
+          </p>
+          <p>Bespoke Brooklyn</p>
+          <p>Calvin Bullock</p>
+          <p>Composers Recordings Incorporated</p>
+          <p>Equitable Investment Corporation</p>
+          <p>Goldman Sachs &amp; Co</p>
+          <p>Honest Diner</p>
+          <p>Johnson &amp; Johnson</p>
+          <p>International Association of Lighting Designers</p>
+          <p>IntoTea</p>
+          <p>Mary McDowell School</p>
+          <p>National Book Foundation</p>
+          <p>Neighborhood Health Partners</p>
+          <p>Nick &amp; Toni’s</p>
+          <p>Financial Guaranty Insurance Company</p>
+          <p>Society of Corporate Secretaries &amp; Governance Professionals</p>
+        </div>
+        <div className="flex flex-col pb-8">
+          <p className="text-secondary font-bold">
+            Development, Events, Exhibits &amp; Signage
+          </p>
+          <p>Bike New York</p>
+          <p>Goldman Sachs &amp; Co</p>
+          <p>Mary McDowell School</p>
+          <p>National Book Foundation, National Book Awards</p>
+          <p>Society of Corporate Secretaries &amp; Governance Professionals</p>
+          <p>Townhouse.bz + Susan Rowland: Uncontained Forces</p>
+          <p>Townhouse.bz + Kamilla Talbot: Getting Outside</p>
+          <p>Townhouse.bz: Faux Real</p>
+          <p>Townhouse.bz: Smackdown - Modernism vs Neoclassical Design</p>
+          <p>Townhouse.bz: Chinoiserie</p>
+          <p>Townhouse.bz: Give Joy</p>
+          <p>Townhouse.bz: Dogs, Men + Light</p>
+        </div>
+        <div className="flex flex-col pb-8">
+          <p className="text-secondary font-bold">
+            Recruiting &amp; Human Resources Campaigns
+          </p>
+          <p>American Home Products</p>
+          <p>Galt &amp; Company</p>
+          <p>Goldman Sachs &amp; Co</p>
+          <p>Marakon Associates</p>
+          <p>Delta Consulting</p>
+          <p>New York Hospital, Weill Cornell Medical Center</p>
+        </div>
+        <div>
+          <img src="./images/1.gif" className="w-[320px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default MeetOurClients;
